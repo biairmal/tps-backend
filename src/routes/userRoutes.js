a4f0e454685e8e5dfe9fa77a487c1a7aef7cdb54
Bandana Irmal A <47946762+biairmal@users.noreply.github.com>
2022-03-21 10:34:12 +0700

    create user validations

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
+const validateRequestSchema = require('../middlewares/validateRequestSchema')
+const userSchema = require('../validations/userSchema')
 const userController = require('../controllers/userController')
 
 const router = Router()
 
 router.route('/user')
-    .post(userController.createUser)
+    .post(userSchema.createUserSchema, validateRequestSchema, userController.createUser)
     .get(userController.getUsers)
 
 module.exports = router
---
+++ src/validations/user-schema.js
@@ -0,0 +1,24 @@
+const { body } = require('express-validator')
+
+const createUserSchema = [
+  body('username')
+    .exists({ checkFalsy: true, checkNull: true })
+    .withMessage('username can not be blank')
+    .bail()
+    .isLength({ min: 6, max: 20 })
+    .withMessage('Username must be between 6-20 characters long')
+    .bail()
+    .matches(/^[A-Za-z0-9]+$/)
+    .withMessage('username can only contains alphabets and numbers'),
+  body('password')
+    .exists({ checkFalsy: true, checkNull: true })
+    .withMessage('password can not be blank'),
+  body('first_name')
+    .exists({ checkFalsy: true, checkNull: true })
+    .withMessage('first_name can not be blank'),
+  body('role')
+    .exists({ checkFalsy: true, checkNull: true })
+    .withMessage('role can not be blank'),
+]
+
+module.exports = { createUserSchema }
